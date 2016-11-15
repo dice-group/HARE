b5b38a3c9fcc82b769fd97d8cbfd203c989ff8e6
micha <michoffmann.potsdam@gmail.com>
2016-11-15 22:05:56 +0100

    new file:   e2i.pkl 	new file:   ekaw.nt 	new file:   example.ttl 	new file:   getTriple2TripleMatrix.py 	new file:   lubm.nt 	new file:   pageRank.py 	new file:   rdf2tensor.py

---
+++ getTriple2TripleMatrix.py
@@ -0,0 +1,51 @@
+import pickle as pkl
+import gc
+import numpy as np
+from scipy import sparse as sparse
+from scipy import io
+from collections import defaultdict
+
+E2I = pkl.load(open("e2i.pkl", "rb"))
+T2I = pkl.load(open("t2i.pkl", "rb"))
+i = 0
+F = sparse.dok_matrix((len(E2I), len(T2I)))
+W = sparse.dok_matrix((len(T2I), len(E2I)))
+f = defaultdict(int)
+w = defaultdict(int)
+print("CONSTRUCTING DICTIONARY OF KEYS")
+k = 0
+for t, i in T2I.items():
+	k += 1
+	if k%1000 == 0:
+		print(k)
+	for e, tmp in E2I.items():
+		j = tmp[0]
+		if e in t:		
+			#edge from entity_j to triple_i detected
+			w[(i, j)] = 1/3
+			f[(j, i)] = 1/E2I[e][1]
+print("UPDATING MATRIX F...")
+for key, val in f.items():
+	F[key] = val
+print("UPDATING MATRIX W...")
+for key, val in w.items():
+	W[key] = val
+print("DELETING DICTIONARY...")
+del f
+del w
+gc.collect()
+del gc.garbage[:]
+print("SAVING MATRIX F...")
+f_coo=F.tocoo()
+row=f_coo.row
+col=f_coo.col
+data=f_coo.data
+shape=f_coo.shape
+np.savez("F",row=row,col=col,data=data,shape=shape)
+print("SAVING MATRIX W...")
+w_coo=W.tocoo()
+row=w_coo.row
+col=w_coo.col
+data=w_coo.data
+shape=w_coo.shape
+np.savez("W",row=row,col=col,data=data,shape=shape)
---
+++ pageRank.py
@@ -0,0 +1,51 @@
+from scipy import sparse as sparse
+from scipy import io
+import numpy as np
+import pickle as pkl
+E2I = pkl.load(open("e2i.pkl", "rb"))
+T2I = pkl.load(open("t2i.pkl", "rb"))
+I2E = dict()
+I2T = dict()
+for key, val in E2I.items():
+	I2E[val[0]] = key
+for key, val in T2I.items():
+	I2T[val] = key
+error = -1
+epsilon = 10**(-3)
+
+y = np.load("F.npz")
+F = sparse.coo_matrix((y['data'],(y['row'],y['col'])),shape=y['shape'])
+
+y = np.load("W.npz")
+W = sparse.coo_matrix((y['data'],(y['row'],y['col'])),shape=y['shape'])
+
+P = sparse.csr_matrix(F.dot(W))
+n = P.shape[0]
+
+previous = np.ones(n)/n
+ones = np.ones(n)
+
+error = 1
+epsilon = 10**(-3)
+damping = 0.85
+
+print(P.todense())
+while error > epsilon:
+	tmp = np.array(previous)
+	previous = damping*P.T.dot(previous) + (1-damping)*ones
+	error = np.linalg.norm(tmp - previous)	
+	print(error)
+
+with open("results_resources.txt", "w") as results:
+	for i, x in sorted(enumerate(previous), key = lambda x: -x[1]):
+		tmp = ""		
+		tmp += str(I2E[i])
+		results.write(tmp +  " " + str(x) + "\n")
+
+tripledistribution = F.T.dot(previous)
+with open("results_triples.txt", "w") as results:
+	for i, x in sorted(enumerate(tripledistribution), key = lambda x: -x[1]):
+		tmp = ""		
+		for n in I2T[i]:
+			tmp += str(n) + " "
+		results.write(tmp +  " " + str(x) + "\n")
