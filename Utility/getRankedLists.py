0bb287f538205dda0bd8e7be96d35680a0adac04
micha <michoffmann.potsdam@gmail.com>
2016-12-27 14:29:08 +0100

    new file:   Utility/getRankedLists.py 	new file:   Utility/prepareEval.py 	new file:   prepare_eval.py

---
+++ Utility/getRankedLists.py
@@ -0,0 +1,34 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+from Utility.config import data_dir
+
+def getRankedLists(classes, method):
+	savepath = data_dir + "RankedLists/" 
+	if method == "HARE":
+		loadpath = data_dir + "Results/" + "results_resources_dbpedia_HARE.txt"	
+	if method == "PAGERANK:
+		loadpath = data_dir + "Results/" + "results_dbpedia_PAGERANK.txt"
+	
+
+
+	for class_name in classes:
+		    print(class_name)
+		    query = """SELECT ?entity
+		               WHERE {?entity a <http://dbpedia.org/ontology/""" + class_name + ">}"
+
+		    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+		    sparql.setQuery(query)
+		    sparql.setReturnFormat(JSON)
+		    results = sparql.query().convert()
+
+		    entities = []
+		    for result in results["results"]["bindings"]:
+		        entities.append(result["entity"]['value'])
+
+		    entities = set(entities)
+		    with open(loadpath, "r") as f, open(savepath + class_name + "_" + method ".txt", "w+") as g:
+		            for line in f:
+		                    line = line.split(" ")
+		                    if line[0] in entities:
+		                            g.write(" ".join(line))
+
+
---
+++ prepare_eval.py
@@ -0,0 +1,12 @@
+from Utility.getRankedLists import getRankedLists
+from Utility.prepareEval import prepareEval
+
+method = "HARE" #PAGERANK
+
+classes = ["City", "University", "VideoGame", "SpaceStation", "Mountain", "Hotel",
+	            "EurovisionSongContestEntry", "Drug", "Comedian", "ChessPlayer",
+	            "Band", "Album", "Person", "Automobile", "HistoricPlace", "Town",	
+	            "MilitaryConflict", "ProgrammingLanguage", "Country"]
+
+getRankedLists(classes, method)
+prepareEval(classes, method)
