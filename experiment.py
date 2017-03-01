841d9ef6a6ca7561dfadf9c1a5d5c7465c1fc421
micha <michoffmann.potsdam@gmail.com>
2017-03-01 14:27:08 +0100

    modified:   experiment.py 	Computations/degree.py

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ from Utility.getTransitionMatrices import getTransitionMatrices
 from Utility.parseRDF import parseRDF
 from Computations.hare import hare
 from Computations.pagerank import pagerank
+from Computations.degree import degree, get_ranks
 import numpy as np
 
 repetitions = 5
@@ -13,13 +14,13 @@ getTransitionMatrices(data)
 print(".....HARE.....")
 runtimes_hare = np.array(repetitions*[.0])
 for i in range(repetitions-1):
-	runtime = hare(data, epsilon=10**(-3), damping = .85, saveresults=False, printerror=False, printruntimes=False)
+	runtime = hare(data, epsilon=10**(-3), damping = .85, saveresults=True, printerror=False, printruntimes=False)
 	runtimes_hare[i] = runtime
 print("Average Runtime HARE: ", np.mean(runtimes_hare))
 
 print(".....PAGERANK.....")
 runtimes_pagerank = np.array(repetitions*[.0])
 for i in range(repetitions):
-	runtime = pagerank(data, epsilon=10**(-3), damping = .85, saveresults=False, printerror=False, printruntimes=False)
+	runtime = pagerank(data, epsilon=10**(-3), damping = .85, saveresults=True, printerror=False, printruntimes=False)
 	runtimes_pagerank[i] = runtime
 print("Average Runtime PAGERANK: ", np.mean(runtimes_pagerank))
