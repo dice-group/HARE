6ae7866bdba21d12a7612739ef28f1a460abb8c0
Ricardo Usbeck <RicardoUsbeck@akswnc10.aksw.uni-leipzig.de>
2017-06-07 09:59:09 +0200

    runtimes meassured after bug fix

---
+++ experiment_scripts.py
@@ -0,0 +1,28 @@
+from Utility.getTransitionMatrices import getTransitionMatrices
+from Utility.parseRDF import parseRDF
+from Computations.hare_scripts import hare
+from Computations.pagerank_scripts import pagerank
+import numpy as np
+
+repetitions = 5
+dataset = ["Airports","dbpedia","dogfood","LUBM/L20","LUBM/L50","LUBM/L100","LUBM/L200","LUBM/L500","LUBM/L1000","sec","sider","UPSTO"]
+for data in dataset:
+
+	data = "Data/Matrices/HARE/" + data
+	print("WITH: ", data)
+	#parseRDF(data)
+	#getTransitionMatrices(data)
+
+	print(".....HARE.....")
+	runtimes_hare = np.array(repetitions*[.0])
+	for i in range(repetitions-1):
+		runtime = hare(data, epsilon=10**(-2), damping = .85, saveresults=False, printerror=False, printruntimes=False)
+		runtimes_hare[i] = runtime
+	print("Average Runtime HARE: ", np.mean(runtimes_hare))
+
+	print(".....PAGERANK.....")
+	runtimes_pagerank = np.array(repetitions*[.0])
+	for i in range(repetitions):
+		runtime = pagerank(data, epsilon=10**(-3), damping = .85, saveresults=False, printerror=False, printruntimes=False)
+		runtimes_pagerank[i] = runtime
+	print("Average Runtime PAGERANK: ", np.mean(runtimes_pagerank))
